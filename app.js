049ded7610e09635dedc91c22754eb7450d7f603
ZakariyeAden <zakariye5417@gmail.com>
2023-05-15 10:13:46 -0500

    explaning what a function is

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 console.log("Hello World");
 
+// ***********************
+// ******* Functions *******
+// ***********************
+
+// Functions are another tool we use to run the same code over and over again without having to duplicate it. We'll start with the basic concept of a function and then move into how to use them in your code.
